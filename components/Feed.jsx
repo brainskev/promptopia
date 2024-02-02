81b36363c22085436d15dfc5eb190d37aa4a188f
Kelvin <ngotho.kelvin@yahoo.com>
2024-02-03 00:54:06 +0300

    Add prompt card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+'use client';
 
-const feed = () => {
-  return (
-    <div>feed</div>
+import {useState, useEffect } from 'react'
+import promptCatd from './PromptCard';
+const PromptCard = ({ data, handleTagClick }) =>
+
+
+const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const handleSearchange = (e) => {
+
+  }
+ return (
+    <section className='feed'>
+      <form className='relative w-full fles-center'>
+        <input
+        type='text'
+        placeholder='Search for a tag or a username'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        />
+      </form>
+      promptCardList
+    </section>
   )
 }
 
