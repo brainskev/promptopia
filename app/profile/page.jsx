db835ab5f1d06bed63d101fdd309f2316b4f84c4
Kelvin <ngotho.kelvin@yahoo.com>
2024-02-06 23:41:21 +0300

    Add fetch prompts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
-const Profile = () => {
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+
+const myProfile = () => {
+
+  useEffect = () => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+
+      setPosts(data);
+    }
+
+    console.log
+  }
+
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+
+    }
+
+
   return (
-    <div>Profile</div>
-  )
-}
+    <Profile
+        name="My"
+        desc="Welcome to your personolized profile page"
+        data={[]}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+     
+    />
+)}
 
-export default Profile
+export default myProfile
