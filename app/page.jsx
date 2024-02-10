51d63e157fbd92da50d6205b99a6ccd8d5251887
Kelvin <ngotho.kelvin@yahoo.com>
2024-02-10 23:57:51 +0300

    Add paragraph

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <span className='orange_gradient'> AI-Powered Promtps</span>
     </h1>
     <p className='desc text-center' >
-       promptia is an open-source AI prompting tool for modern world to discover, create and share prompts
+       promptia is an open-source AI prompting tool for modern world to discover, create and share creative Promtps prompts
     </p>
   
    <Feed/>
